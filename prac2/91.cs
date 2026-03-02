d0d43356913c82843bdffa06e4b22b61890450e8
Suksham <sukshamdafure@gmail.com>
2026-03-02 16:59:51 +0530

    prac2

---
+++ prac2/91.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[][] jagged = new int[3][];
+
+        jagged[0] = new int[] { 1, 2 };
+        jagged[1] = new int[] { 3, 4, 5 };
+        jagged[2] = new int[] { 6 };
+
+        for (int i = 0; i < jagged.Length; i++)
+        {
+            for (int j = 0; j < jagged[i].Length; j++)
+            {
+                Console.Write(jagged[i][j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
