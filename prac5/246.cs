953e103ee2aec3f6088474e30e1758299794aeaf
Suksham <sukshamdafure@gmail.com>
2026-04-20 14:33:26 +0530

    246

---
+++ prac5/246.cs
@@ -0,0 +1,48 @@
+using System;
+
+class HourglassPattern
+{
+    static void Main()
+    {
+        int rows;
+
+        Console.Write("Enter number of rows: ");
+        rows = int.Parse(Console.ReadLine());
+
+        // Upper Inverted Pyramid
+        for (int i = rows; i >= 1; i--)
+        {
+            // Spaces
+            for (int j = rows; j > i; j--)
+            {
+                Console.Write(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        // Lower Pyramid
+        for (int i = 2; i <= rows; i++)
+        {
+            // Spaces
+            for (int j = i; j < rows; j++)
+            {
+                Console.Write(" ");
+            }
+
+            // Stars
+            for (int k = 1; k <= (2 * i - 1); k++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
