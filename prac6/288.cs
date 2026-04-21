73a170c0e37ac7ae0fc0006ce5a63b5b0a670acc
Suksham <sukshamdafure@gmail.com>
2026-04-21 13:45:27 +0530

    288

---
+++ prac6/288.cs
@@ -0,0 +1,43 @@
+using System;
+
+class BubbleSort
+{
+    static void Main()
+    {
+        int n;
+
+        Console.Write("Enter size of array: ");
+        n = int.Parse(Console.ReadLine());
+
+        int[] arr = new int[n];
+
+        Console.WriteLine("Enter elements:");
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Bubble Sort
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    // Swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine("Sorted array:");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+    }
+}
